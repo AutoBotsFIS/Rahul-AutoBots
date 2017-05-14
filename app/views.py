80c7e9baff84fc7baee924e9fdec7e092e0db54d
AutoBotsFIS <mohak.wathare@yahoo.com>
2017-05-14 20:31:49 +0530

    Update views.py
    Added code to read json from request and send response in text format ####Check this code###

--- app/views.py
+++ app/views.py
@@ -1,6 +1,50 @@
 from app import app
 
-@app.route('/')
-@app.route('/index')
+
+
+#@app.route('/')
+#@app.route('/index')
+@app.route('/webhook', methods=['POST'])
+
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+	if req.get("result").get("action") == "index":
+		result = req.get("result")
+		#parameters = result.get("parameters")
+		speech=index()
+		
+				
+		
+	print("Response:")
+	print(speech)
+	return {
+	"speech": speech,
+	"displayText": speech,
+    #"data": {},
+    # "contextOut": [],	
+	"source": "apiai-Web-Search-1"
+	}
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
+
+
 def index():
     return "Hello, World!"
